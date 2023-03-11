76d6a962655afde1729d200a2b48c242f2d27dec
ayanokoujifl <guleite3@gmail.com>
2023-03-11 19:31:38 -0300

    .

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-window.onload = () => {
+window.onload = async () => {
   const container = document.querySelector('#characters-container')
   let arr = []
 
   for (i = 4; i <= 44; i++) {
     arr.push(
-      fetch(
-        `https://gateway.marvel.com/v1/public/series/${i}?ts=1&apikey=d8c3803ccd948ebabbc4da84fda16dd0&hash=57bc55842c425353bd5b9e88e262c7fc&limit=20`
+      await fetch(
+        `https://gateway.marvel.com/v1/public/series/${i}?ts=1&apikey=d8c3803ccd948ebabbc4da84fda16dd0&hash=57bc55842c425353bd5b9e88e262c7fc`
       ).then((response) => {
         return response.json()
       })
@@ -17,8 +17,18 @@ window.onload = () => {
   fetchSeries.then((data) => {
     data.map((serie) => {
       let div = document.createElement('div')
-      div.innerHTML = `<img src="${serie.data.results[0].thumbnail.path}.${serie.data.results[0].thumbnail.extension}" alt="" />
+      div.innerHTML = `<img src="${serie.data.results[0].thumbnail.path}.${
+        serie.data.results[0].thumbnail.extension
+      }" alt="" />
+        <p>
+        ${serie.data.results[0].title}<br>${
+        serie.data.results[0].description
+          ? serie.data.results[0].description
+          : serie.data.results[0].resourceURI
+      }
+      </p>
       `
+
       container.append(div)
     })
   })
